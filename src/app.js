fb987e05dae59cf8bdb3a9aa104efef64e9a3770
sedi <salehisediqe86@gmail.com>
2023-01-24 08:11:22 -0800

    Called the API Forecast

--- src/app.js
+++ src/app.js
@@ -13,7 +13,8 @@ let day= days[date.getDay()];
 return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast(){
+function displayForecast(response){
+    console.log(response.data.daily);
     let forecastElement = document.querySelector("#forecast");
     let forecastHTML= "";
     let days = ["Thu","Fri","Sat","Sun"];
@@ -40,12 +41,13 @@ forecastHTML= forecastHTML + `
    
 }
 
-
-
-
-
-
-
+function getForecast(coordinates){
+console.log(coordinates);
+let apiKey = "cad4a7b2655c670bbf4e9139ebd662ce";
+let apiUrl = `https://pro.openweathermap.org/data/2.5/forecast/hourly?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+//console.log(apiUrl);
+axios.get(apiUrl).then(displayForecast);
+}
 
 
 
@@ -69,6 +71,9 @@ windElement.innerHTML = Math.round(response.data.wind.speed);
 dateElement.innerHTML = formatDate(response.data.dt * 1000);
 iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 iconElement.setAttribute("alt", response.data.weather[0].description);
+
+getForecast(response.data.coord);
+
 }
 
 function search(city){
@@ -92,4 +97,3 @@ let celsiusTemperature = null;
 
  
  search("New York");
-displayForecast();
